a9081ed1832e8c88a28f2189ec9e39193440a263
Farhan Maniya <farhanmaniya3578@gmail.com>
2026-06-11 11:48:44 +0530

    both url changed again

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://night-bite-ciy5.vercel.app/",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
 // Automatically attach token to every request
