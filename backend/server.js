ec7964f66e6fed18e9ad92573000ed66aa8c6fab
Farhan Maniya <farhanmaniya3578@gmail.com>
2026-06-11 11:45:40 +0530

    both url changed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://night-bite-azure.vercel.app/"],
     credentials: true,
   }),
 );
